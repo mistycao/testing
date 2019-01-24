915388a594f7eeec8f6a8abffb1f1e011be010e0
mistycao <46795136+mistycao@users.noreply.github.com>
2019-01-23 22:48:04 -0600

    created dollar.c
    added existing code

---
+++ dollar.c
@@ -0,0 +1,26 @@
+/*
+ *  File Name: dollar.c
+ *  Author: Misty Cao78
+ *  Date: 1/23/2019
+ * 
+ * Description: Converts US dollars to Britsh Pounds and Japenses JPY.
+ * 
+ */
+#include <stdio.h>. 
+
+int main(int argc, char **argv) {
+
+    double usDollarAmt;     //Setting up variables
+    double britishPoundsAmt;
+    double japanYenAmt;
+
+    printf("Please input the total amount of US Dollars: ");
+    scanf("%lf", &usDollarAmt);     //User enters amount of US dollars
+    usDollarAmt = 0.9 * usDollarAmt;        //Exchange fee of 10% and subsquent calcualations
+    britishPoundsAmt = (usDollarAmt / 2) * 0.6;
+    japanYenAmt = (usDollarAmt / 2) * 76.8;
+    printf("You get %.2f British Pounds and %.2f Japanese JPY", britishPoundsAmt, japanYenAmt);
+
+
+return 0;
+}
